ef7b9f8434f2dd86e94d3549777bbd3c825360e5
Clifton <cliftonreed1986@gmail.com>
2024-05-07 14:25:40 -0700

    insureing updates caught

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const playlistTrackController = require('./playlistTrackController');
 
+
 router.use('/api', apiRoutes);
 router.use('/', playlistTrackController);
 
