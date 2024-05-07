30d0a3115f80360ce61141e6ffe79756e1dccae3
Clifton <cliftonreed1986@gmail.com>
2024-05-06 23:47:16 -0700

    added notes and some code resorces to the createplaylist.js file.

--- public/js/createplaylist.js
+++ public/js/createplaylist.js
@@ -3,35 +3,68 @@
     <h2>Create Playlist</h2>
 
     <section class="createPlaylist">
-    <form action="#" method="get">
+    <form action="" method="get">
         <label for="genre">Genre</label>
         <input type="text" class="genre" name="genre">
-        <button type="submit" id="genreBtn">🔍</button><br><br>
+        <button type="submit" id="genreBtn">🔍</button>
     
         <label for="artist">Artist</label>
         <input type="text" class="artist" name="artist">
-        <button type="submit" id="artistBtn">🔍</button><br><br>
+        <button type="submit" id="artistBtn">🔍</button>
     
         <label for="album">Album</label>
         <input type="text" class="album" name="album">
-        <button type="submit" id="albumBtn">🔍</button><br><br>
+        <button type="submit" id="albumBtn">🔍</button>
     </form>
     </section>
-
 </div>
 
 
-// Step 1: Identify the Event
+//addEventListene
 const genreButton = document.getElementById('genreBtn');
 const artistButton = document.getElementById('artistBtn');
 const albumButton = document.getElementById('albumBtn');
 
-// Step 3: Write the Event Listener Function
+
 function handleClick() {
   console.log('search initiated....');
 }
 
-// Step 4: Attach the Event Listener
+
 genreButton.addEventListener('click', handleClick);
 artistButton.addEventListener('click', handleClick);
 albumButton.addEventListener('click', handleClick);
+
+{/* /////////////////////////////////////// */}
+{/* Song list for playlist that have been created created */}
+
+<ul class="song_list">
+  {{#each song}}
+    <li>{{this}}</li>
+  {{/each}}
+</ul>
+
+
+{/* ///////////////////////////////////// */}
+
+
+<div>
+    <h2>Recommendations</h2>
+    <section id="searchResults">
+        {/* <!-- Results will be rendered here --> */}
+    </section>
+</div>
+
+
+<div>
+    <h2>Selected Songs</h2>
+
+    <ul id="selectedSongs">
+        {/* <!-- Selected songs will be added here --> */}
+    </ul>
+</div>
+
+
+
+{/* ///////////////////////////////// */}
+
