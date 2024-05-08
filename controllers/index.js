4c37e8cbf0d158f140faa4e1396eed47b8adedf1
Gabriela Ortiz <gabrielaortiz@Gabrielas-Air.lan>
2024-05-08 12:54:30 -0700

    i removed the references to playlistTrackController since we nuced it

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const playlistTrack = require('./playlistTrackController');
 const homepage = require('./homepageController');
 
 router.use('/', homepage);
-router.use('/playlistTrack', playlistTrack);
 router.use('/api', apiRoutes);
 
 
